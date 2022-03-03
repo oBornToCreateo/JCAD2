c17d327b04e4c93f01dbd0627b793689261c820f
oBornToCreateo <Kjezier@gmail.com>
2022-03-03 06:49:23 +0100

    Project Setup

---
+++ JCAD2/WinMain.cpp
@@ -0,0 +1,28 @@
+#include "Window.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hIstance,
+	HINSTANCE hPrevIstancce,
+	LPSTR	  lpCmdLine,
+	int       nCmdShow)
+
+{
+	Window wnd(800, 300, "Donkey Fart Box");
+
+	//Message Loop
+	MSG msg;
+	BOOL gResult;
+	while (gResult=(GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+}
