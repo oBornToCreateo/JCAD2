d56bb8aea7e948cb6b746e0ef393cb008d935bd1
oBornToCreateo <Kjezier@gmail.com>
2022-03-03 13:10:44 +0100

    finnishing error handling

--- JCAD2/WinMain.cpp
+++ JCAD2/WinMain.cpp
@@ -7,22 +7,39 @@ int CALLBACK WinMain(
 	int       nCmdShow)
 
 {
-	Window wnd(800, 300, "JCAD 2");
+	try
+	{
 
-	//Message Loop 
-	MSG msg;
-	BOOL gResult;
-	while (gResult=(GetMessage(&msg, nullptr, 0, 0)) > 0)
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-	if (gResult == -1)
-	{
-		return -1;
-	}
-	else
-	{
+			Window wnd(800, 300, "JCAD 2");
+
+			//Message Loop 
+			MSG msg;
+			BOOL gResult;
+			while (gResult=(GetMessage(&msg, nullptr, 0, 0)) > 0)
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		if (gResult == -1)
+		{
+			return -1;
+			throw CHWND_LAST_EXCEPT();
+		}
+		
 		return msg.wParam;
+		
 	}
+	catch (const ChiliException& e)
+	{
+		MessageBox(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (const std::exception& e)
+	{
+		MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
 }
--- JCAD2/Window.h
+++ JCAD2/Window.h
@@ -69,3 +69,4 @@ private:
 
 // error exception helper macro
 #define CHWND_EXCEPT( hr ) Window::Exception( __LINE__,__FILE__,hr ) 
+#define CHWND_LAST_EXCEPT() Window::Exception( __LINE__,__FILE__,GetLastError() )
